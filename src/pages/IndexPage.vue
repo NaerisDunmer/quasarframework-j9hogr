564a51634959bc5619cfdc8d2c90a50efb662858
NaerisDunmer <128970344+NaerisDunmer@users.noreply.github.com>
2023-03-26 12:56:17 +0200

    1 file modified

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,39 +1,6 @@
 <template>
   <q-page class="column flex-center">
-    hello ther
-
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 140px"
-    />
-
-    <div class="q-mt-xl">
-      <q-btn
-        color="primary"
-        dense
-        round
-        label="-"
-        :disable="count === min"
-        @click="count--"
-      />
-
-      <span class="q-mx-md text-bold">{{ count }}</span>
-
-      <q-btn
-        color="primary"
-        dense
-        round
-        label="+"
-        :disable="count === max"
-        @click="count++"
-      />
-    </div>
-
-    <div class="q-mt-md" style="width: 200px">
-      <q-slider v-model="count" :min="min" :max="max" />
-    </div>
-  </q-page>
+    hello there
 </template>
 
 <script setup>
