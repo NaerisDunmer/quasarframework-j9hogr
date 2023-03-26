f22f235411c298b38229b6b2b1eb8e7d7e9d3324
NaerisDunmer <128970344+NaerisDunmer@users.noreply.github.com>
2023-03-26 13:22:10 +0200

    1 file modified

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -41,52 +41,16 @@ import EssentialLink from 'components/EssentialLink.vue';
 
 const linksList = [
   {
+    title: 'Registrierung',
+    caption: 'Neues Konto anlegen',
+    icon: 'school',
+    link: '',
+  },
+  {
     title: 'Wer hilft bei Problemen?',
     caption: 'Support kontaktieren',
     icon: 'record_voice_over',
     link: 'https://www.hochschule-ruhr-west.de/schnelleinstieg/kontakt/',
-  },
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev',
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework',
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev',
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev',
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev',
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev',
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev',
   },
 ];
 
